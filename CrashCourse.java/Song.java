b63a53fcc3512fd5b1fc5defcd99c80843131160
JKo12345-theskibber <kochan20280717@student.nhvweb.net>
2026-09-18 14:06:26 +0000

    oop 2 final

---
+++ CrashCourse.java/Song.java
@@ -0,0 +1,45 @@
+public class Song {
+    // private instancce variables
+    private String title;
+    private String artist;
+    private String album;
+    private int lengthInSeconds;
+    private int releaseYear;
+    private String genre;
+    private boolean isExplicit;
+    private int plays;
+   
+    // constructor
+    public Song(String title, String artist, int lengthInSeconds) {
+        this.title = title;
+        this.artist = artist;
+        this.lengthInSeconds = lengthInSeconds;
+        album = "Unknown";
+        releaseYear = 2024;
+        genre = "Unclassified";
+        isExplicit = false;
+        plays = 0;
+    }
+   
+    // methods
+    public void play() {
+        plays = plays + 1;
+        System.out.println(title + " by " + artist + " is now playing.");
+    }
+   
+    public void increasePlayCount() {
+        plays = plays + 1;
+    }
+   
+    public void markAsFavorite() {
+        if (isExplicit == true) {
+            System.out.println(title + " is explicit but marked as favorite.");
+        } else {
+            System.out.println(title + " marked as favorite.");
+        }
+    }
+   
+    public void getArtistInfo() {
+        System.out.println("Artist: " + artist + " | Song plays: " + plays);
+    }
+}
